6a2104723d687a7f18b931c37d6c11a4b808ed72
pcwhite <philupthecup87@gmail.com>
2025-09-25 18:23:01 -0400

    first commit for project

---
+++ nextjs/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="bg-red-500 text-white">
+        Hello from my new next.js app
+      </div>
+      <Button>Click me</Button>
+    </div>
+  );
+}
