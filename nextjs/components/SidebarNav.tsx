b8039e67ec1b631ae55c8b8c91b059214d0dff51
pcwhite <philupthecup87@gmail.com>
2025-09-28 17:10:17 -0400

    Sidebar.tsx and SidebarNav.tsx refactors

--- nextjs/components/SidebarNav.tsx
+++ nextjs/components/SidebarNav.tsx
@@ -1,69 +1,72 @@
-import React from 'react'
-import { Button } from './ui/button';
-import Link from 'next/link';
-import { Home, Settings, LayoutDashboard, LucideIcon } from 'lucide-react';
-import { cn } from '@/lib/utils';
-import { usePathname } from 'next/navigation';
+import React from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { Home, Settings, LayoutDashboard, LucideIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 
 interface SidebarNavProps {
-    isMobile: boolean;
-    isCollapsed: boolean;
+  isCollapsed: boolean;
 }
 
 interface NavItem {
-    label: string;
-    href: string;
-    icon: LucideIcon;
-    isActive: (pathname: string) => boolean;
+  label: string;
+  href: string;
+  icon: LucideIcon;
+  isActive: (pathname: string) => boolean;
 }
 
-    export default function SidebarNav({ isMobile, isCollapsed }: SidebarNavProps) {
+export default function SidebarNav({ isCollapsed }: SidebarNavProps) {
+  const pathname = usePathname();
 
-    const pathname = usePathname();
-
-    const navItems: NavItem[] = [
-        {
-            label: 'Projects',
-            href: '/projects',
-            icon: Home,
-            isActive: (pathname: string) =>
-                pathname === '/projects' || pathname.startsWith('/projects/')
-        },
-        {
-            label: 'Templates',
-            href: '/templates',
-            icon: LayoutDashboard,
-            isActive: (pathname: string) => pathname === '/templates' || pathname.startsWith('/templates/')
-        },
-        {
-            label: 'Settings',
-            href: '/settings',
-            icon: Settings,
-            isActive: (pathname: string) => pathname === '/settings' || pathname.startsWith('/settings/')
-        }
-    ]
+  const navItems: NavItem[] = [
+    {
+      label: "Projects",
+      href: "/projects",
+      icon: Home,
+      isActive: (pathname: string) =>
+        pathname === "/projects" || pathname.startsWith("/projects/"),
+    },
+    {
+      label: "Templates",
+      href: "/templates",
+      icon: LayoutDashboard,
+      isActive: (pathname: string) =>
+        pathname === "/templates" || pathname.startsWith("/templates/"),
+    },
+    {
+      label: "Settings",
+      href: "/settings",
+      icon: Settings,
+      isActive: (pathname: string) =>
+        pathname === "/settings" || pathname.startsWith("/settings/"),
+    },
+  ];
 
   return (
     <div className="space-y-4 overflow-hidden mb-auto">
-    {navItems.map(item => (
+      {navItems.map((item) => (
         <Button
-            key={item.href}
-            variant="ghost"
-            asChild
-            className={cn(
-                "w-full justify-start hover:text-main hover:bg-gray-200 flex items-center text-lg font-normal",
-                !isMobile && isCollapsed && "justify-center p-2",
-                item.isActive(pathname) && "text-main bg-gray-200"
+          key={item.href}
+          variant="ghost"
+          asChild
+          className={cn(
+            "w-full justify-start hover:text-main hover:bg-gray-200 flex items-center text-lg font-normal",
+            isCollapsed && "lg:justify-center lg:p-2",
+            item.isActive(pathname) && "text-main bg-gray-200"
+          )}
+        >
+          <Link href={item.href}>
+            <item.icon className="h-[22px] w-[22px]" />
+            {/* DESKTOP STYLES */}
+            {!isCollapsed && (
+              <span className="ml-3 hidden lg:inline">{item.label}</span>
             )}
-            >
-                <Link href={item.href}>
-                    <item.icon className="h-[22px] w-[22px]" />
-                    {(isMobile || !isCollapsed) && (
-                        <span className="ml-3">{item.label}</span>
-                    )}
-                </Link>
-            </Button>
-        ))}
+            {/* MOBILE STYLES */}
+            <span className="ml-3 lg:hidden">{item.label}</span>
+          </Link>
+        </Button>
+      ))}
     </div>
-  )
+  );
 }
