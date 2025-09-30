26eca0ac21bac98f8ea4b919c5c5c2022be4401b
pcwhite <philupthecup87@gmail.com>
2025-09-29 23:20:51 -0400

    Refactor ProjectsPage to fetch projects from server and implement ProjectList component for a button to create a New Project

--- nextjs/app/(dashboard)/projects/page.tsx
+++ nextjs/app/(dashboard)/projects/page.tsx
@@ -1,29 +1,37 @@
-import React from 'react'
-
-type Project = {
-    name: string
-    description: string
-}
+import { createProject } from "@/server/mutations";
+import { getProjectsForUser } from "@/server/queries";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import React from "react";
+import ProjectList from "@/components/ProjectList";
 
 export default async function ProjectsPage() {
-const projectPromise = new Promise<Project[]>((resolve) => {
-    setTimeout(() => {
-        resolve([
-            {name: 'Project 1', description: 'Description 1'},
-            {name: 'Project 2', description: 'Description 2'},
-            {name: 'Project 3', description: 'Description 3'},
-        ])
-    }, 5000)
-})
+  const projects = await getProjectsForUser();
 
-const projects = await projectPromise;
+  return (
+    <div className="w-full">
+      {/* TODO: Create projects list header with create action */}
+      {/* TODO: Grid of cards to view each project and navigate to detail view */}
 
-return (
-    <div>
-        <h1>Projects</h1>
-        {projects.map((project, idx) => (
-            <div key={idx}>{project.name}</div> 
-        ))}
+      <div className="max-w-screen-2xl mx-auto p-4 sm:p-6 md:p-8 lg:p-12 mt-2 space-y-6 sm:space-y-8 lg:space-y-10">
+        <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-4 sm:mb-6">
+          <div className="space-y-2 sm:space-y-4 mb-4 sm:mb-0">
+            <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold">
+              My Projects
+            </h1>
+            <p className="text-sm sm:text-base text-gray-500">
+              Manage your Marketing projects effectively using AI
+            </p>
+          </div>
+          <form action={createProject} className="w-full sm:w-auto">
+            <Button className="rounded-3xl text-base w-full sm:w-auto">
+              <Plus className="w-4 h-4 mr-1" strokeWidth={3} />
+              New Project
+            </Button>
+          </form>
+        </div>
+        <ProjectList projects={projects} />
+      </div>
     </div>
-  )
+  );
 }
