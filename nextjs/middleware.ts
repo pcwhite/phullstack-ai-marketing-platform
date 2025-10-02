39f1ad040617cc9a4e4566c2a35664a2fba7bab1
pcwhite <philupthecup87@gmail.com>
2025-10-02 17:18:19 -0400

    Fixed api/upload middleware

--- nextjs/middleware.ts
+++ nextjs/middleware.ts
@@ -1,11 +1,11 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 // TODO: Add public and private routes
-const isPublicRoute = createRouteMatcher(['/', '/pricing']);
+const isPublicRoute = createRouteMatcher(["/", "/pricing", "/api/upload"]);
 
 export default clerkMiddleware(async (auth, req) => {
-    // if a user is not authenticated and they are trying to
-    // access a private route, redirect to Clerk login page
+  // if a user is not authenticated and they are trying to
+  // access a private route, redirect to Clerk login page
   if (!(await auth()).userId && !isPublicRoute(req)) {
     await (await auth()).redirectToSignIn();
   }
@@ -14,8 +14,8 @@ export default clerkMiddleware(async (auth, req) => {
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
